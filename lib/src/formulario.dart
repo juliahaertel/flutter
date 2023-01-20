3b24b29d268361a7228642886823d296f8464861
Julia Ballen Haertel <juuliaballenhaertel@gmail.com>
2023-01-20 13:11:59 -0300

    Adicionado validações formulario

--- lib/src/formulario.dart
+++ lib/src/formulario.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FormularioPage extends StatefulWidget{
@@ -9,6 +10,10 @@ class FormularioPage extends StatefulWidget{
 }
   class _Formulario extends State<FormularioPage>{
     final _formkey = GlobalKey<FormState>();
+
+    String email = '';
+    String nome  = '';
+    //String senha = '';
     
   @override
   Widget build(BuildContext context){
@@ -16,6 +21,8 @@ class FormularioPage extends StatefulWidget{
       key: _formkey,
       child:Column(
         children: [
+          //const SizedBox(height:50),
+          const Padding(padding: EdgeInsets.only(top: 10.0)),
           TextFormField(
             decoration: const InputDecoration(
               hintText: 'Digite seu nome:'
@@ -27,7 +34,13 @@ class FormularioPage extends StatefulWidget{
                 null;
               }
             },
+            onSaved: (txt){
+              setState(() {
+                nome = txt!;
+              });
+            },
           ),
+          const Padding(padding: EdgeInsets.only(top: 25.0)),
           TextFormField(
             decoration: const InputDecoration(
               hintText: 'Digite seu email:'
@@ -39,14 +52,52 @@ class FormularioPage extends StatefulWidget{
                 null;
               }
             },
+            onSaved: (txt){
+              setState(() {
+                email = txt!;
+              });
+            },
           ),
+          const Padding(padding: EdgeInsets.only(top: 25.0)),
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'Digite sua senha:'
+            ),
+            validator: (value){
+              if(value!.isEmpty){
+                return 'Informe a senha!';
+              }else if(value.length < 8){
+                return 'Informe uma senha com no minimo 8 digitos.';
+              }else{
+                null;
+              }
+            },
+          ),
+          const Padding(padding: EdgeInsets.only(top: 10.0)),
           ElevatedButton(
             child: const Text('Enviar'),
             onPressed: (){
               if(_formkey.currentState!.validate()){
+                _formkey.currentState!.save();
                 print("validadooow");
               }
             },
+          ),
+          const Padding(padding: EdgeInsets.only(top: 70.0)),
+          Container(
+            //color: Colors.lightBlue,
+            height: 70,
+            //width: 100000,
+            child: Row(
+              children: [
+                Expanded(child: ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text('Usuario registrado: \nNome $nome e Email $email'),
+                  )
+                )
+              ],
+            )
           )
         ],
       ),
